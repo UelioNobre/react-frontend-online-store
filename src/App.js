eff0f784dbe02e2c33354102b9975a8ab38bd743
uelionobre <uelionobre@hotmail.com>
2022-11-15 12:04:20 -0300

    adicionando header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Products from './Components/Products';
-import Shopcart from './Pages/Shopcart';
+import Shopcart from './pages/Shopcart';
 import Details from './Components/Details';
 
 function App() {
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>Formulário de busca</div>
+          <div>Logo</div>
+          <div>button cart</div>
+        </div>
+      </div>
+    );
+  }
+}
